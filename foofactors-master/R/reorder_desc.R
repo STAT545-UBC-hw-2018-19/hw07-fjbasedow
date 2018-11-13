495d503c02b10ca413aca983620d2385ecd03c10
Frederike Basedow <f.basedow@arcor.de>
2018-11-13 12:04:54 -0800

    add dependencies

--- foofactors-master/R/reorder_desc.R
+++ foofactors-master/R/reorder_desc.R
@@ -5,15 +5,19 @@
 #' @param x a factor (or character vector)
 #' @param y a vector of the same length as x, based on which levels of x will be ordered.
 #' If y is a character vector, it will be sorted in reverse alphabetic order and x ordered accordingly.
+#' @param FUN a function to be applied to each subset of y, determined by the levels of x. Default is mean.
+#' @param ... extra arguments supplied to FUN, optional
 #'
 #' @examples
 #' x <- c("a", "d", "h", "g", "m")
 #' y <- c(1, 3, 9, 7, 6)
 #' reorder_desc(x, y)
 #'
+#' reorder_desc(iris$Species, iris$Sepal.Width, median)
+#'
 #' @export
 
-reorder_desc <- function(x, y){
-  reversed <- reorder(x, dplyr::desc(y))
+reorder_desc <- function(x, y, FUN = mean, ...){
+  reversed <- reorder(x, dplyr::desc(y), FUN = FUN)
   return(reversed)
 }
