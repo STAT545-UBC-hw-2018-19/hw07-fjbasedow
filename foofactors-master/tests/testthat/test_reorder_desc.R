ef1a251f41dec350d8067ff185478098517e6877
Frederike Basedow <f.basedow@arcor.de>
2018-11-14 22:20:08 -0800

    add test 3

--- foofactors-master/tests/testthat/test_reorder_desc.R
+++ foofactors-master/tests/testthat/test_reorder_desc.R
@@ -9,7 +9,7 @@ test_that("reorder_desc reorders levels of x based on decreasing mean of y", {
   expect_identical(levels(reorder_desc(x, y)), levels(z))
 })
 
-test_that("reorder_desc calculates summary stat correct", {
+test_that("reorder_desc calculates mean correctly", {
   f <- factor(c("a", "a", "a", "b", "b", "b"))
   g <- c(1,2,3,4,5,6)
   data_2 <- data.frame(f, g)
@@ -20,3 +20,13 @@ test_that("reorder_desc calculates summary stat correct", {
   expect_identical(-h$mean[2], attr(m, "scores")[[2]])
 })
 
+test_that("reorder_desc calculates other stat correctly", {
+  l <- factor(c("a", "a", "a", "b", "b", "b"))
+  k <- c(1,6,3,9,5,6)
+  data_2 <- data.frame(l, k)
+  o <- data_2 %>% dplyr::group_by(l) %>% dplyr::summarize(median = median(k))
+  p <- reorder_desc(l, k, median)
+  
+  expect_identical(-o$median[1], attr(p, "scores")[[1]])
+  expect_identical(-o$median[2], attr(p, "scores")[[2]])
+})
