a0a1eb1544aa49e82e6e2ff4697bf47d1e15f3ac
Frederike Basedow <f.basedow@arcor.de>
2018-11-14 22:14:39 -0800

    add test 2

--- foofactors-master/tests/testthat/test_reorder_desc.R
+++ foofactors-master/tests/testthat/test_reorder_desc.R
@@ -9,3 +9,14 @@ test_that("reorder_desc reorders levels of x based on decreasing mean of y", {
   expect_identical(levels(reorder_desc(x, y)), levels(z))
 })
 
+test_that("reorder_desc calculates summary stat correct", {
+  f <- factor(c("a", "a", "a", "b", "b", "b"))
+  g <- c(1,2,3,4,5,6)
+  data_2 <- data.frame(f, g)
+  h <- data_2 %>% dplyr::group_by(f) %>% dplyr::summarize(mean = mean(g))
+  m <- reorder_desc(f, g)
+
+  expect_identical(-h$mean[1], attr(m, "scores")[[1]])
+  expect_identical(-h$mean[2], attr(m, "scores")[[2]])
+})
+
