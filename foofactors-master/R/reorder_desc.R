7ded16cbda1267dd4ed43b63b3d443a014fdfcd3
Frederike Basedow <f.basedow@arcor.de>
2018-11-15 19:00:07 -0800

    change text in reorder_desc function

--- foofactors-master/R/reorder_desc.R
+++ foofactors-master/R/reorder_desc.R
@@ -1,6 +1,7 @@
 #' Reorder levels of a factors in descending order
 #'
-#' Descending version of the `reorder` function. Reorders levels of a factor based on descending order of values of a second variable (usually numeric).
+#' Descending version of the `reorder` function.
+#' Reorders levels of a factor based on descending order of values of a second variable (usually numeric).
 #'
 #' @param x a factor (or character vector)
 #' @param y a vector of the same length as x, based on which levels of x will be ordered.
@@ -8,7 +9,8 @@
 #' @param FUN a function to be applied to each subset of y, determined by the levels of x. Default is mean.
 #' @param ... extra arguments supplied to FUN, optional
 #'
-#' @return A factor with the order of the levels determined by FUN applied to y grouped by x. The levels are ordered such that the values returned by FUN are in descreasing order.
+#' @return A factor with the order of the levels determined by FUN applied to y grouped by x.
+#' The levels are ordered such that the values returned by FUN are in descreasing order.
 #'
 #' @examples
 #' x <- c("a", "d", "h", "g", "m")
